a788f1810fa2e6f25b361ddae820bd54e07019b0
ExpansiveGold <gabriel.tarumoto@gmail.com>
2023-11-23 20:38:59 -0800

    Started site responsiveness

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ window.onload = function date() {
     document.getElementById("data").value = dateNow
     console.log(dateNow);
 }
+
+const element = document.getElementById("columns");
+
+if (screen.width < 800) {
+    setTimeout(() => {
+        element.className = "row row-cols-2 mb-3"
+    }, 100); 
+} 
+else if (screen.width > 801) {
+    setTimeout(() => {
+        element.className = "row row-cols-4 mb-3"
+    }, 100); 
+}   
